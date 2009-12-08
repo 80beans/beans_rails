98aa0c354d3fd3b987ea5e6f246b6d750652b282
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2009-12-08 14:35:20 +0100

    more cleaning up

--- beans_rails.rb
+++ beans_rails.rb
@@ -6,51 +6,38 @@ class Rails::TemplateRunner
   end
   
   def template_file(template, replace = {})
-    replace = {:app_name => app_name}.merge(replace)
-    contents = open(File.join(TEMPLATES_DIR, template)).read
-        
-    replace.keys.each do |key|
-      contents.gsub!("{{#{key.to_s}}}", replace[key])
-    end
-    
-    file template, contents
+    replace   = {:app_name => app_name}.merge(replace)
+    contents  = open(File.join(TEMPLATES_DIR, template)).read
+    replace.keys.each { |key| contents.gsub!("{{#{key.to_s}}}", replace[key]) }
+    file(template, contents)
   end
 end
 
 # removing unnecessary files
 
-run "rm README"
-run "rm doc/*"
-run "rm log/*"
-run "rm -rf test"
-run "rm public/index.html"
-run "rm public/images/rails.png"
+['README', 'doc/*', 'log/*', 'test', 'public/index.html', 'public/images/rails.png'].each { |file| run "rm -rf #{file}" }
 
-# gems
+# installing gems
 
-gem 'haml'
-gem 'mislav-will_paginate', :lib => 'will_paginate',  :source => 'http://gems.github.com'
-gem 'thoughtbot-paperclip', :lib => 'paperclip',      :source => 'http://gems.github.com'
+['haml', 'will_paginate', 'paperclip'].each { |rubygem| gem rubygem }
+rake "gems:install", :sudo => true
 
-# generating haml & rspec stuff
+# generating rspec stuff
 
-run "haml --rails ."
 generate :rspec
 
-# updating the database file
+# database config & gitignore
 
-template_file('config/database.yml')
+['config/database.yml', '.gitignore'].each { |file| template_file(file) }
 
 # capistrano
 
 if yes?('Add capistrano configuration?')
   template_file('Capfile')
-  template_file('config/deploy', {:server => ask('What is the server name?')})
+  template_file('config/deploy', { :server => ask('What is the server name?') })
 end
 
-# git 
-
-template_file('.gitignore')
+#git 
 
 git :init
 git :add => "."
@@ -58,5 +45,5 @@ git :commit => "-a -m 'Initial commit'"
 
 if yes?("Push to remote git repository? (The repository #{app_name} must be configured in Gitosis)") then
   git :remote => "add origin git@git.80beans.net:#{app_name}.git"
-  git :push => 'origin master:refs/heads/master'
+  git :push   => 'origin master:refs/heads/master'
 end
